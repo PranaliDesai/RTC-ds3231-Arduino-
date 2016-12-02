7d6cedb7cb837d2b9958dbd7497922b36abea006
PranaliDesai <pranali.yashodhar@gmail.com>
2016-12-02 12:03:38 +0300

    Add files via upload

---
+++ rtc/rtc.ino
@@ -0,0 +1,51 @@
+// Date and time functions using a RX8025 RTC connected via I2C and Wire lib
+
+#include <Wire.h>
+#include "Sodaq_DS3231.h"
+
+int i ; 
+char weekDay[][4] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+//year, month, date, hour, min, sec and week-day(starts from 0 and goes to 6)
+//writing any non-existent time-data may interfere with normal operation of the RTC.
+//Take care of week-day also.
+//DateTime dt(2016, 11, 4, 6, 22, 0, 5);
+
+
+void setup () 
+{
+    Serial.begin(9600);
+    Wire.begin();
+    rtc.begin();
+    //rtc.setDateTime(dt); //Adjust date-time as defined 'dt' above 
+}
+
+void loop () 
+{
+    DateTime now = rtc.now(); //get the current date-time
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.date(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    Serial.print(weekDay[now.dayOfWeek()]);
+    Serial.println();
+   
+    delay(1000);
+   
+
+   
+    
+
+
+
+
+    
+}
